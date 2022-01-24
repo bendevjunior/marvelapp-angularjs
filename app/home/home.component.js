69ec119d1f67403597318e9d25ae18e3ca874387
Bendev junior <bendevoficial@gmail.com>
2022-01-24 15:39:35 -0400

    adicionando paginacao e busca

--- app/home/home.component.js
+++ app/home/home.component.js
@@ -1,20 +1,42 @@
 'use strict';
 
 angular.
-  module('home').
-  component('homeComponent', {
-    templateUrl: 'home/home.template.html',
-    controller: ['Hero',
-      function HomeController(Hero) {
-        this.loading = true
-        Promise.resolve(Hero.query())
-        .then(value => {
-          this.loading = false
-          this.marvel = value 
-          console.log(value)
-        })
-        .catch(e => console.log(e))
-        
+  module('home')
+  .filter('startFrom', function () {
+    return function (input, start) {
+      if (input) {
+        start = +start;
+        return input.slice(start);
       }
+      return [];
+    };
+  })
+  .component('homeComponent', {
+    templateUrl: 'home/home.template.html',
+    controller: ['$scope', 'Hero',
+      function HomeController($scope, Hero) {
+        this.loading = true
+
+        $scope.currentPage = 0;
+        $scope.pageSize = 10;
+        $scope.q = '';
+        Promise.resolve(Hero.query())
+          .then(value => {
+            this.loading = false
+            this.marvel = value
+            console.log(value)
+          })
+          .catch(e => console.log(e))
+
+        $scope.numberOfPages = function (totalItems) {
+          return Math.ceil(totalItems / $scope.pageSize);
+        }
+        $scope.$watch('q', function (newValue, oldValue) {
+          if (oldValue != newValue) {
+            $scope.currentPage = 0;
+          }
+        }, true);
+      }
+
     ]
   });
